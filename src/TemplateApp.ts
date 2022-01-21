02ddf3ec291fd001d71ca78c415f63baa4681f8e
josfeenstra <me@josfeenstra.nl>
2022-01-21 16:41:01 +0100

    removed things from root

--- src/TemplateApp.ts
+++ src/TemplateApp.ts
@@ -1,12 +1,12 @@
 
-import {App, Camera, DebugRenderer, DrawSpeed, InputHandler, InputState, LineShader, MultiLine, MultiVector3, Parameter, Plane, Scene, UI, Vector3} from "Geon";
+import {App, Camera, DebugRenderer, DrawSpeed, InputHandler, InputState, LineShader, Mesh, MultiLine, MultiVector3, Parameter, Plane, Scene, UI, Vector3} from "Geon";
 
 export class TemplateApp extends App {
 
     // render
-    scene: Scene;
-    mr: DebugRenderer;
-    gs: LineShader;
+    scene: Scene; // a scene is a simple collection of lights, the sun, and a camera. This is what we will feed to shaders at render time
+    gs: LineShader; // an abstract shader. The 'set' method shows what it needs to fill its buffers. This one requires a 'MultiLine'. 
+    debug: DebugRenderer; // this is a collection of shaders, automatically created to quickly debug things. This is not the most efficient in the world, so please, use it only for debugging :).
 
     constructor(gl: WebGLRenderingContext) {
         super(gl);
@@ -16,7 +16,7 @@ export class TemplateApp extends App {
         
         this.scene = new Scene(camera);
         this.gs = new LineShader(gl, [0.3, 0.3, 0.3, 1]);
-        this.mr = DebugRenderer.new(gl);
+        this.debug = DebugRenderer.new(gl);
     }
 
     start() {
@@ -32,6 +32,8 @@ export class TemplateApp extends App {
     startGrid() {
         let grid = MultiLine.fromGrid(Plane.WorldXY().moveTo(new Vector3(0, 0, -1)), 100, 2);
         this.gs.set(grid, DrawSpeed.StaticDraw);
+
+        this.debug.set(Mesh.newCylinder(Vector3.new(0,0,0), Vector3.new(0,0,1), 1, 20), "unit");
     }
 
     update(input: InputHandler) {
@@ -40,6 +42,6 @@ export class TemplateApp extends App {
 
     draw() {
         this.gs.render(this.scene);
-        this.mr.render(this.scene);
+        this.debug.render(this.scene);
     }
 }
